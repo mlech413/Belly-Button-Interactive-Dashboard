a4f359384ed181c3db9859fe816e1334e016e104
mlech413 <mlech413@gmail.com>
2023-04-01 19:30:47 -0400

    demographics

--- static/js/app.js
+++ static/js/app.js
@@ -6,12 +6,12 @@ function init(){
             selector.append("option").text(names[i]).property("value", names[i]);
         }
         let firstSample = names[0];
-        chart(firstSample);
+        charts(firstSample);
         metadata(firstSample);
     })
 }
 
-function chart(sample){
+function charts(sample){
     d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
         let samples = data.samples;
         let resultArray = samples.filter((sampleDictionary) => sampleDictionary.id == sample);
@@ -38,11 +38,36 @@ function chart(sample){
             }
         ]
         Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+
+        let barData = [
+            {
+                x: sampleValues.slice(0, 10).reverse(),
+                y: otuIDs.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+                text: otuLabels.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h"
+            }
+        ]
+
+        let barLayout = {
+            margin: {t: 0, l: 100}
+        }
+        Plotly.newPlot("bar", barData, barLayout)
     });
 }
 
-function metadata(){
+function metadata(sample){
 
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let metadata = data.metadata;
+        let resultArray = metadata.filter(sampleDictionary => sampleDictionary.id == sample);
+        let result = resultArray[0];
+        let panel = d3.select("#sample-metadata");
+        panel.html("");
+        for(key in result){
+            panel.append("h6").text(`${key.toUpperCase()}: ${result[key]}`)
+        }
+    })
 }
 
 init();
